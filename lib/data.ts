d3ef99e3610a22a6e1c8f11419942e0fc99f6a70
mydeenok <mydeen.pitchai@obiikriationz.com>
2025-05-21 11:34:29 +0530

    Refactor code structure for improved readability and maintainability

--- lib/data.ts
+++ lib/data.ts
@@ -515,107 +515,107 @@ export const PROJECTS: IProject[] = [
             '/projects/images/blazers-io-2.webp',
         ],
     },
-    {
-        title: 'Epikcart',
-        slug: 'epikcart',
-        techStack: [
-            'React',
-            'Redux',
-            'React i18n',
-            'Tailwind CSS',
-            'Framer Motion',
-            'debouncing',
-            'Api Integration',
-        ],
-        thumbnail: '/projects/thumbnail/epikcart.jpg',
-        longThumbnail: '/projects/long/epikcart.jpg',
-        images: [
-            '/projects/images/epikcart-1.png',
-            '/projects/images/epikcart-2.png',
-            '/projects/images/epikcart-3.png',
-            '/projects/images/epikcart-4.png',
-            '/projects/images/epikcart-5.png',
-        ],
-        liveUrl: 'https://demo.epikcart.siphertech.com/',
-        year: 2023,
-        description: `Epikcart is a feature-rich, scalable e-commerce platform tailored for large businesses. It features dynamic product filtering, multi-language support with RTL, advanced inventory management, order tracking, and refund systems, offering a comprehensive solution for multi-vendor operations.`,
-        role: `As the Full Stack Developer in a team of five, I: <br/>
-        - Built the frontend from scratch using React, Redux, RTK Query, and Tailwind CSS.<br/>
-        - Developed dynamic filtering logic for the product search page with admin-configurable parameters.<br/>
-        - Integrated multi-language support with React i18n, including RTL handling.<br/>
-        - Delivered a responsive, user-friendly interface in collaboration with the UI/UX designer.`,
-    },
-    {
-        title: 'Resume Roaster',
-        slug: 'resume-roaster',
-        techStack: [
-            'GPT-4',
-            'Next.js',
-            'Postgressql',
-            'Prisma',
-            'Tailwind CSS',
-        ],
-        thumbnail: '/projects/thumbnail/resume-roaster.jpg',
-        longThumbnail: '/projects/long/resume-roaster.jpg',
-        images: [
-            '/projects/images/resume-roaster-1.png',
-            '/projects/images/resume-roaster-2.png',
-            '/projects/images/resume-roaster-3.png',
-        ],
-        liveUrl: 'https://resume-roaster.vercel.app/',
-        year: 2023,
-        description:
-            'Resume Roaster is a web application designed to provide tailored resume feedback and professional writing services. Built with Next.js, PostgreSQL, Prisma, and Tailwind CSS, it integrates GPT-4 for AI-powered recommendations. The platform also includes peer-to-peer reviews with a points-based system, fostering a collaborative and engaging experience. Targeting freshers, experienced professionals, and programmers, it helps optimize resumes for job-specific success.',
-        role: `As the sole developer and business owner, I:<br/>
-        - Designed and developed the platform end-to-end using Next.js, PostgreSQL, Prisma, and Tailwind CSS.<br/>
-        - Integrated GPT-4 for AI-driven feedback and insights.<br/>
-        - Implemented complex SQL queries, including one to identify the top two resumes based on user points.`,
-    },
-    {
-        title: 'Real Estate',
-        slug: 'property-pro',
-        techStack: [
-            'React.js',
-            'Redux',
-            'Tailwind CSS',
-            'React i18n',
-            'Framer Motion',
-        ],
-        thumbnail: '/projects/thumbnail/property-pro.jpg',
-        longThumbnail: '/projects/long/property-pro.jpg',
-        images: [
-            '/projects/images/property-pro-1.png',
-            '/projects/images/property-pro-2.png',
-            '/projects/images/property-pro-3.png',
-        ],
-        liveUrl: 'https://demo.propertypro.siphertech.com/',
-        year: 2023,
-        description:
-            'PropertyPro is a real estate management platform offering users a seamless experience to explore, manage, and view property listings. The application emphasizes accessibility and responsive design, ensuring a smooth interface across devices.',
-        role: `As the Full Stack Developer, I:<br/>
-        - Built the frontend using React, Redux, RTK Query, Framer Motion, and Tailwind CSS.<br/>
-        - Integrated dynamic state management for efficient handling of property data.<br/>
-        - Implemented multi-language support with React i18n to cater to diverse audiences.<br/>
-        - Enhanced user interaction with animations and transitions using Framer Motion.`,
-    },
-    {
-        title: 'Consulting Finance',
-        slug: 'crenotive',
-        techStack: ['HTML', 'CSS & SCSS', 'Javascript', 'Bootstrap'],
-        thumbnail: '/projects/thumbnail/consulting-finance.jpg',
-        longThumbnail: '/projects/long/consulting-finance.jpg',
-        images: [
-            '/projects/images/consulting-finance-1.png',
-            '/projects/images/consulting-finance-2.png',
-            '/projects/images/consulting-finance-3.png',
-        ],
-        sourceCode: 'https://github.com/Tajmirul/crenotive',
-        liveUrl: 'https://crenotive.netlify.app/',
-        year: 2023,
-        description:
-            'I developed Crenotive, a portfolio website using Html, SASS, and jQuery to showcase services and expertise. The design focuses on responsive user experience and effective presentation of professional achievements.',
-        role: ``,
-    },
+    // {
+    //     title: 'Epikcart',
+    //     slug: 'epikcart',
+    //     techStack: [
+    //         'React',
+    //         'Redux',
+    //         'React i18n',
+    //         'Tailwind CSS',
+    //         'Framer Motion',
+    //         'debouncing',
+    //         'Api Integration',
+    //     ],
+    //     thumbnail: '/projects/thumbnail/epikcart.jpg',
+    //     longThumbnail: '/projects/long/epikcart.jpg',
+    //     images: [
+    //         '/projects/images/epikcart-1.png',
+    //         '/projects/images/epikcart-2.png',
+    //         '/projects/images/epikcart-3.png',
+    //         '/projects/images/epikcart-4.png',
+    //         '/projects/images/epikcart-5.png',
+    //     ],
+    //     liveUrl: 'https://demo.epikcart.siphertech.com/',
+    //     year: 2023,
+    //     description: `Epikcart is a feature-rich, scalable e-commerce platform tailored for large businesses. It features dynamic product filtering, multi-language support with RTL, advanced inventory management, order tracking, and refund systems, offering a comprehensive solution for multi-vendor operations.`,
+    //     role: `As the Full Stack Developer in a team of five, I: <br/>
+    //     - Built the frontend from scratch using React, Redux, RTK Query, and Tailwind CSS.<br/>
+    //     - Developed dynamic filtering logic for the product search page with admin-configurable parameters.<br/>
+    //     - Integrated multi-language support with React i18n, including RTL handling.<br/>
+    //     - Delivered a responsive, user-friendly interface in collaboration with the UI/UX designer.`,
+    // },
+    // {
+    //     title: 'Resume Roaster',
+    //     slug: 'resume-roaster',
+    //     techStack: [
+    //         'GPT-4',
+    //         'Next.js',
+    //         'Postgressql',
+    //         'Prisma',
+    //         'Tailwind CSS',
+    //     ],
+    //     thumbnail: '/projects/thumbnail/resume-roaster.jpg',
+    //     longThumbnail: '/projects/long/resume-roaster.jpg',
+    //     images: [
+    //         '/projects/images/resume-roaster-1.png',
+    //         '/projects/images/resume-roaster-2.png',
+    //         '/projects/images/resume-roaster-3.png',
+    //     ],
+    //     liveUrl: 'https://resume-roaster.vercel.app/',
+    //     year: 2023,
+    //     description:
+    //         'Resume Roaster is a web application designed to provide tailored resume feedback and professional writing services. Built with Next.js, PostgreSQL, Prisma, and Tailwind CSS, it integrates GPT-4 for AI-powered recommendations. The platform also includes peer-to-peer reviews with a points-based system, fostering a collaborative and engaging experience. Targeting freshers, experienced professionals, and programmers, it helps optimize resumes for job-specific success.',
+    //     role: `As the sole developer and business owner, I:<br/>
+    //     - Designed and developed the platform end-to-end using Next.js, PostgreSQL, Prisma, and Tailwind CSS.<br/>
+    //     - Integrated GPT-4 for AI-driven feedback and insights.<br/>
+    //     - Implemented complex SQL queries, including one to identify the top two resumes based on user points.`,
+    // },
+    // {
+    //     title: 'Real Estate',
+    //     slug: 'property-pro',
+    //     techStack: [
+    //         'React.js',
+    //         'Redux',
+    //         'Tailwind CSS',
+    //         'React i18n',
+    //         'Framer Motion',
+    //     ],
+    //     thumbnail: '/projects/thumbnail/property-pro.jpg',
+    //     longThumbnail: '/projects/long/property-pro.jpg',
+    //     images: [
+    //         '/projects/images/property-pro-1.png',
+    //         '/projects/images/property-pro-2.png',
+    //         '/projects/images/property-pro-3.png',
+    //     ],
+    //     liveUrl: 'https://demo.propertypro.siphertech.com/',
+    //     year: 2023,
+    //     description:
+    //         'PropertyPro is a real estate management platform offering users a seamless experience to explore, manage, and view property listings. The application emphasizes accessibility and responsive design, ensuring a smooth interface across devices.',
+    //     role: `As the Full Stack Developer, I:<br/>
+    //     - Built the frontend using React, Redux, RTK Query, Framer Motion, and Tailwind CSS.<br/>
+    //     - Integrated dynamic state management for efficient handling of property data.<br/>
+    //     - Implemented multi-language support with React i18n to cater to diverse audiences.<br/>
+    //     - Enhanced user interaction with animations and transitions using Framer Motion.`,
+    // },
+    // {
+    //     title: 'Consulting Finance',
+    //     slug: 'crenotive',
+    //     techStack: ['HTML', 'CSS & SCSS', 'Javascript', 'Bootstrap'],
+    //     thumbnail: '/projects/thumbnail/consulting-finance.jpg',
+    //     longThumbnail: '/projects/long/consulting-finance.jpg',
+    //     images: [
+    //         '/projects/images/consulting-finance-1.png',
+    //         '/projects/images/consulting-finance-2.png',
+    //         '/projects/images/consulting-finance-3.png',
+    //     ],
+    //     sourceCode: 'https://github.com/Tajmirul/crenotive',
+    //     liveUrl: 'https://crenotive.netlify.app/',
+    //     year: 2023,
+    //     description:
+    //         'I developed Crenotive, a portfolio website using Html, SASS, and jQuery to showcase services and expertise. The design focuses on responsive user experience and effective presentation of professional achievements.',
+    //     role: ``,
+    // },
     {
         title: 'devLinks',
         slug: 'devLinks',
