d520ebf9a3460a5b9910f1428f5b573411586a6b
mydeenok <mydeen.pitchai@obiikriationz.com>
2025-05-27 11:46:27 +0530

    feat: implement root layout with optimized font loading and analytics setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,20 +51,18 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <head>
-            {/* Google Tag Manager */}
-<Script id="gtm-script" strategy="afterInteractive">
-  {`
-    (function(w,d,s,l,i){
-      w[l]=w[l]||[];
-      w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-      var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-      j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-      f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-NRPMNQP6');
-  `}
-</Script>
-{/* <!-- End Google Tag Manager --> */}
+                {/* Google Tag Manager */}
+                <Script id="gtm-script" strategy="afterInteractive">
+                    {`
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-NRPMNQP6');
+                    `}
+                </Script>
+                {/* End Google Tag Manager */}
+
                 {/* Preload critical assets */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -80,15 +78,13 @@ export default function RootLayout({
                 `}} />
 
             </head>
-            {/* Google Tag Manager (noscript) */}
-<noscript dangerouslySetInnerHTML={{
-  __html: `
-    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NRPMNQP6" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-  `
-}} />
             <body
                 className={`${antonFont.variable} ${robotoFlex.variable} antialiased`}
             >
+                {/* Google Tag Manager (noscript) */}
+                <noscript dangerouslySetInnerHTML={{
+                    __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NRPMNQP6" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+                }} />
                 <ThemeProvider>
                     <ReactLenis
                         root
