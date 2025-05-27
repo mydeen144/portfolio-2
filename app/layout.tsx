868bf47f22767af4a9e939f7c01b3169167242b6
mydeenok <mydeen.pitchai@obiikriationz.com>
2025-05-27 11:17:44 +0530

    feat: create root layout with theme support, smooth scroll, and Google Analytics integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,20 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <head>
+            {/* Google Tag Manager */}
+<Script id="gtm-script" strategy="afterInteractive">
+  {`
+    (function(w,d,s,l,i){
+      w[l]=w[l]||[];
+      w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NRPMNQP6');
+  `}
+</Script>
+{/* <!-- End Google Tag Manager --> */}
                 {/* Preload critical assets */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -64,18 +78,14 @@ export default function RootLayout({
                         font-display: optional;
                     }
                 `}} />
+
             </head>
-            <GoogleAnalytics gaId="G-MHLY1LNGY5" />
-            <Script id="hotjar" strategy="lazyOnload">
-                {`(function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:6380611,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-            </Script>
+            {/* Google Tag Manager (noscript) */}
+<noscript dangerouslySetInnerHTML={{
+  __html: `
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NRPMNQP6" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+  `
+}} />
             <body
                 className={`${antonFont.variable} ${robotoFlex.variable} antialiased`}
             >
